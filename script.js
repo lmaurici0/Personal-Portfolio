2eec7015cc7219624cedc6b077ca792b76d3bfcc
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2023-12-12 22:45:23 -0300

    Update jQuery

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 $(document).ready(function () {
     function adjustLayout() {
         var screenWidth = $(window).width();
-        var $projects = $('.projects')
+        var $card = $('.card');
+        var $h1 = $("h1")
+
         if (screenWidth < 600) {
-            $projects.addClass('vertical-layout')
+            $card.addClass('d-flex flex-column justify-content-center align-items-center');
+            $card.css('margin-top', "10px")
+            $h1.removeClass("display-1")
+            $h1.css("font-size","5rem")
         } else {
-            $projects.removeClass('vertical-layout')
+            $card.removeClass('d-flex flex-column justify-content-center align-items-center');
+            $h1.addClass("display-1")
         }
     }
-    adjustLayout()
+
+    adjustLayout();
+
     $(window).resize(function () {
-        adjustLayout()
-    })
-})
+        adjustLayout();
+    });
+});
