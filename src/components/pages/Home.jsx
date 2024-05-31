fc6ae08f323250440c7e46492ac06dc83a6f7620
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2024-05-31 10:18:17 -0300

    feat: add header

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,17 +4,22 @@ import Introduce from '../layout/Introduce'
 import ProjectsNavbar from '../layout/ProjectsNavbar'
 import SectionSkill from '../layout/SectionSkill'
 import Footer from '../layout/Footer'
+import Header from '../layout/Header'
+
+
 
 function Home(){
     return(
         <>
+            <Header />
+        
             <main>
-
                 <Introduce />
                 <SectionSkill /> 
                 <ProjectsNavbar />
-                <Footer />
             </main>
+
+            <Footer />
         </>
     )
 }
