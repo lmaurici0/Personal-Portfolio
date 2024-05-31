a0592c9f6d97080d707acba9adbb2922a1c01c15
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2024-05-31 10:30:32 -0300

    feat: add description name

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 import styles from "../../assets/css/Card.module.css";
 
-function Card({ logo, title }) {
+function Card({ logo, title, text }) {
   return (
       <div className={styles.card_body}>
         <img src={logo} title={title} className={styles.skill_pic} />
+        <p className={styles.name}>{text}</p>
     </div>
   );
 }
